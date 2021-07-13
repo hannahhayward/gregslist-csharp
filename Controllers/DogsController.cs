2a074740ae99adcb9a79e6eee5ff1d037048f134
hannah hayward <hannahhayward97@gmail.com>
2021-07-13 15:37:27 -0600

    dogs done pre postman

--- Controllers/DogsController.cs
+++ Controllers/DogsController.cs
@@ -10,12 +10,46 @@ namespace gregslist_csharp.Controllers
     public class DogsController: ControllerBase{
       private readonly DogsService _ds;
 
-      public DogsController(DogsService ds){
+      public DogsController(DogsService ds)
+      {
         _ds = ds;
       }
     [HttpGet]
-    public List<Dog> GetDogs(){
+    public List<Dog> GetDogs()
+    {
       return _ds.GetAll();
     }
+    [HttpPost]
+    public ActionResult<Dog> CreateDog([FromBody] Dog dogData){
+      try
+      {
+        var dog = _ds.createDog(dogData);
+        return Created("api/dogs/" + dog.Id, dog);
+      }
+      catch(System.Exception e)
+      {
+        return BadRequest(e.Message);
+      }
     }
+    [HttpPut("{id}")]
+    public ActionResult<Dog> EditDog(int id, [FromBody] Dog dogData){
+      try{
+        var dog = _ds.editDog(id, dogData);
+        return dog;
+      }
+      catch (System.Exception e){
+        return BadRequest(e.Message);
+      }
+    }
+    [HttpDelete("{id}")]
+    public ActionResult<Dog> RemoveDog(int id){
+      try{
+        var dog = _ds.removeDog(id);
+        return  Ok(dog);
+      }
+      catch (System.Exception e){
+        return BadRequest(e.Message);
+      }
+    }
+  }
 }
--- Services/DogsService.cs
+++ Services/DogsService.cs
@@ -1,8 +1,36 @@
+using System;
+using System.Collections.Generic;
 using gregslist_csharp.Controllers;
+using gregslist_csharp.Data;
+using gregslist_csharp.Models;
 
 namespace gregslist_csharp.Services
 {
     public class DogsService{
-      
+      public List<Dog> GetAll(){
+        return FakeDb.Dogs;
+      }
+      public Dog createDog(Dog dogData){
+        var r = new Random();
+        dogData.Id = r.Next(100, 10000);
+        FakeDb.Dogs.Add(dogData);
+        return dogData;
+      }
+      public Dog editDog(int id, Dog dogData){
+        var dog = FakeDb.Dogs.Find(d => d.Id == id);
+        if(dog == null){
+          throw new Exception("not a dog");
+        }
+        FakeDb.Dogs.Add(dogData);
+        return dog;
+      }
+      public Dog removeDog(int id){
+        var dog = FakeDb.Dogs.Find(d => d.Id == id);
+        if (dog == null){
+          throw new Exception("not a dog");
+        }
+        FakeDb.Dogs.Remove(dog);
+        return dog;
+      }
     }
 }
